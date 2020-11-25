19970576374c46cc9e480d4036189e86cc77f794
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-11-26 02:13:28 +0330

    chore: bump to react 17

--- examples/web/config-overrides.js
+++ examples/web/config-overrides.js
@@ -7,17 +7,30 @@ const resolveApp = (relativePath) => path.resolve(__dirname, relativePath);
 const appIncludes = [resolveApp("./src"), resolveApp("../../src")];
 
 module.exports = function override(config, env) {
+  const __DEV__ = env !== "production";
+
   // allow importing from outside of src folder
   config.resolve.plugins = config.resolve.plugins.filter(
     (plugin) => plugin.constructor.name !== "ModuleScopePlugin",
   );
 
   config.module.rules[0].include = appIncludes;
-  config.module.rules[1] = null;
-  config.module.rules[2].oneOf[1].include = appIncludes;
-  config.module.rules = config.module.rules.filter(Boolean);
-  config.plugins.push(
-    new webpack.DefinePlugin({ __DEV__: env !== "production" }),
-  );
+  //eslint
+  // if (__DEV__) {
+  //   config.module.rules[1].include = srcIncludes;
+  //   config.module.rules[1].use[0].options.baseConfig = require("./.eslintrc.json");
+  // } else {
+  //   config.module.rules[1] = null;
+  // }
+  // oneOf index will change to 2 in future
+  config.module.rules[1].oneOf[2].include = appIncludes;
+  // config.module.rules[1].oneOf[2].options.plugins = [
+  //   require.resolve("babel-plugin-react-native-web"),
+  // ].concat(config.module.rules[1].oneOf[2].options.plugins);
+  // config.module.rules = config.module.rules.filter(Boolean);
+
+  // config.plugins = config.plugins.filter((v) => !(v instanceof ESLintPlugin));
+
+  config.plugins.push(new webpack.DefinePlugin({ __DEV__ }));
   return config;
 };
